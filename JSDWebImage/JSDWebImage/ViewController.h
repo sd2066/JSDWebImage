7e94d82ac6c8b5854b8a54517da189525e5c10cb
sd2066 <18354205615@163.com>
2016-09-17 18:29:02 +0800

    创建项目

---
+++ JSDWebImage/JSDWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSDWebImage
+//
+//  Created by Abner on 16/9/17.
+//  Copyright © 2016年 Abner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSDWebImage/JSDWebImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSDWebImage
+//
+//  Created by Abner on 16/9/17.
+//  Copyright © 2016年 Abner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
